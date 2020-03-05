9f001d84054b5e157293582c7ed5e04b159d87c4
Eric Platon <zaraki@gmx.com>
2020-03-05 15:36:43 +0900

    Straightforward setup script for AWS Linux instances.

---
+++ devops/aws_linux_install.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Docker
+yum install docker \
+            git
+service docker start
+usermod -a -G docker ec2-user
+chkconfig docker on
+
+# Docker Compose
+curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" \
+  -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+# CVAT
+git clone https://github.com/ic/cvat
+pushd cvat
+git checkout develop # TODO, specify the commit/tag.
+docker-compose build
+docker-compose up -d
+docker exec -it cvat bash -ic 'python3 ~/manage.py createsuperuser'
+
+echo "Good to go on this server at port 8080"
